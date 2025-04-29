f35335d9226b5ce9dc74d4f07fa14e9e77ef0c39
miu1217 <hyun671217@daum.net>
2025-04-30 00:48:53 +0900

    git settings 폴더

---
+++ config/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *  # noqa
+
+DEBUG = True    # 디버그 모드(개발 모드) 에러가 발생 하면 장고에서 노란 화면으로 알려줌
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django_Main',
+        'USER': 'dev_user',
+        'PASSWORD': 'securepassword',
+        'HOST': 'localhost',
+        'PORT': '54322',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ROTATE_REFRESH_TOKENS': True,
+}
---
+++ config/settings/prod.py
@@ -0,0 +1,18 @@
+from .base import * # noqa
+
+DEBUG = False   # 디버그 모드(개발 모드) 에러가 발생 하면 장고에서 노란 화면으로 알려줌
+
+ALLOWED_HOSTS = [
+    "13.125.186.81", # EC2 퍼블릭 IP
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django_Main',
+        'USER': 'dev_user',
+        'PASSWORD': 'securepassword',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
