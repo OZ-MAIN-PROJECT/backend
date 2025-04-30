4f8078956bbee66d6ea41e930bc6d3c60a9ce2f6
miu1217 <hyun671217@daum.net>
2025-04-30 13:57:08 +0900

    ALLOWED_HOSTS 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -4,9 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-8zjl)bzv5vqgb04s-e=))l2_*nctad71t%$ko=od)w47gy%uz8'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.93.163.29", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
