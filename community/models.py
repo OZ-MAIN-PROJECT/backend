da6164be6e3d5c75e9fba6b4230b426d6bc929d5
hyunyoung97 <gusdud04164@gmail.com>
2025-05-02 17:37:58 +0900

    community/models.py 수정

--- community/models.py
+++ community/models.py
@@ -63,16 +63,30 @@ class Comment(models.Model):
 class CommunityLike(models.Model):
     like_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='likes')
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, null=True, related_name='community_likes')
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True, related_name='comment_likes')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('user', 'community')  # 중복으로 좋아요 불가
         db_table = 'community_like'
+        # 게시글+댓글에 중복 좋아요 방지
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'community'], name='unique_user_community_like'),
+            models.UniqueConstraint(fields=['user', 'comment'], name='unique_user_comment_like'),
+        ]
 
     def __str__(self):
-        return f"{self.user} likes {self.community}"
+        if self.community:
+            return f"{self.user} likes post {self.community}"
+        elif self.comment and self.comment.parent_comment:
+            return f"{self.user} likes reply {self.comment}"
+        elif self.comment:
+            return f"{self.user} likes comment {self.comment}"
+        # 게시글,댓글 모두 연결되지 않은 경우 예외 처리 (데이터 이상 가능성)
+        else:
+            return f"{self.user} likes unknown"
     
+
 # 조회수 기록
 class CommunityView(models.Model):
     view_id = models.AutoField(primary_key=True)
@@ -81,8 +95,11 @@ class CommunityView(models.Model):
     viewed_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_togeter = ('user', 'community') # 중복으로 조회수 카운트 불가
         db_table = 'community_view'
+        # 중복 조회 방지
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'community'], name='unique_user_community_view')
+        ]
 
     def __str__(self):
         return f"{self.user} viewed {self.community}"
