7120270bdd39cb614cf6bc24eb02fd11c8e5d47f
miu1217 <hyun671217@daum.net>
2025-05-09 05:05:22 +0900

    sentry 연동

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,9 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -47,5 +50,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("swagger-editor/", TemplateView.as_view(template_name="swagger.html")),
 
-
+    path('sentry-debug/', trigger_error),
 ] + static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'static'))
