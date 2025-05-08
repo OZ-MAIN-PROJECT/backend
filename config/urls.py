f15d86793dc8642b42df7fac73c4634a28d84223
kimjisooooo <9729935@naver.com>
2025-05-08 09:51:38 +0900

    feat: implement community post model and DB setup

--- config/urls.py
+++ config/urls.py
@@ -42,4 +42,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("swagger-editor/", TemplateView.as_view(template_name="swagger.html")),
 
+
 ] + static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'static'))
