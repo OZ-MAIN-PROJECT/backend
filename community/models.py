ea8536a85d14832e018c30c6070d9b93ad512032
miu1217 <hyun671217@daum.net>
2025-05-08 16:59:24 +0900

    community migrate

--- community/models.py
+++ community/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
+
+class CommunityType(models.TextChoices):
+    INFORMATION = 'information', '정보'
+    QNA = 'qna', '질문'
+
+
 class Community(models.Model):
-    TYPE_CHOICES = [
-        ("information", "정보"),
-        ("qna", "질문"),
-    ]
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    type = models.CharField(max_length=20, choices=CommunityType.choices)
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def str(self):
+    def __str__(self):
         return f"[{self.type}] {self.title}"
-
-
-class CommunityType(models.TextChoices):
-    INFORMATION = 'information', '정보'
-    QNA         = 'qna',         '질문'
