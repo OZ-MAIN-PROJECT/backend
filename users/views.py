e8110aa9e104b76fa70bb6d5b2d2806f100c3f25
hanyul-jeong <hanyul0417@gmail.com>
2025-05-12 07:58:45 +0900

    로그아웃, 회원 탈퇴, 비밀번호 찾기

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework.permissions import IsAuthenticated
 from users.models import User
 from django.utils import timezone
 from rest_framework.permissions import AllowAny
@@ -47,3 +48,40 @@ class LoginView(APIView):
             })
         else:
             return Response({'error': '로그인 실패'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "로그아웃 완료"}, status=status.HTTP_205_RESET_CONTENT)
+        except TokenError:
+            return Response({"error": "유효하지 않은 토큰입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+class WithdrawView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response({"message": "회원 탈퇴 완료"}, status=status.HTTP_204_NO_CONTENT)
+
+class PasswordResetVerifyView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get("email")
+        question = request.data.get("question")
+        answer = request.data.get("answer")
+
+        try:
+            user = User.objects.get(email=email)
+            if user.question != question or user.answer != answer:
+                return Response({"error": "입력하신 내용이 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+            return Response({"message": "확인되었습니다."}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"error": "해당 이메일의 사용자가 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
