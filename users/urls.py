cc87ef454fc83a6b4df1e474fda8ec98dabe1dc6
hanyul-jeong <hanyul0417@gmail.com>
2025-05-13 16:40:08 +0900

    엔드포인트 재 설정

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('withdraw/', WithdrawView.as_view(), name='withdraw'),
-    path('password-verify/', PasswordResetVerifyView.as_view(), name='password_verify'),
+    path('find-password/', PasswordResetVerifyView.as_view(), name='password_verify'),
+    
+    path('mypage/', WithdrawView.as_view(), name='withdraw'), #탈퇴는 임시
 ]
