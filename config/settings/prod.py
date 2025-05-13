817aee3888e223ded5b71ef8b1147ca4c8bac001
miu1217 <hyun671217@daum.net>
2025-05-13 20:24:45 +0900

    prod.py 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import * # noqa
+import os
 
 DEBUG = False   # 디버그 모드(개발 모드) 에러가 발생 하면 장고에서 노란 화면으로 알려줌
 
@@ -16,3 +17,5 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
