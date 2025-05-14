55e668813f6d655a934ccf7eaadb550e0bc0c009
miu1217 <hyun671217@daum.net>
2025-05-15 03:08:00 +0900

    전체적인 models 수정

--- community/models.py
+++ community/models.py
@@ -9,41 +9,54 @@ class CommunityType(models.TextChoices):
 
 
 class Community(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
     type = models.CharField(max_length=20, choices=CommunityType.choices)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
 
     def __str__(self):
         return f"[{self.type}] {self.title}"
 
+    class Meta:
+        db_table = 'community'
+
 class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
     community = models.ForeignKey(
         Community,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        db_column='community_id'
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        db_column='user_id'
     )
     content = models.TextField()
 
     # parent가 None이면 최상위 댓글, 있으면 그 댓글의 대댓글
-    parent = models.ForeignKey(
+    parentCommentId = models.ForeignKey(
         'self',
         null=True,
         blank=True,
         related_name='replies',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        db_column='parent_comment_id'
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def str(self):
         return f"{self.user} on {self.community} – {self.content[:20]}"
+
+    class Meta:
+        db_table = 'comment'
