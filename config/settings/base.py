aeef7bd797d4c0ba85e684dd4dfde1ae15f95a39
miu1217 <hyun671217@daum.net>
2025-05-16 17:42:16 +0900

    cors url 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,7 +91,8 @@ TEMPLATES = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://localhost:5173",
-    "https://sussyoo.netlify.app"
+    "https://sussyoo.netlify.app",
+    "https://sussyoo-dev.netlify.app"
 ]
 
 # 인증 요청(Credentials: include)을 허용하려면
