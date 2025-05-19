ba316e94821c24f439b1a05a501f4e23dd094788
hyunyoung97 <gusdud04164@gmail.com>
2025-05-19 16:03:46 +0900

    renderse 수정

--- common/renderers.py
+++ common/renderers.py
@@ -4,6 +4,6 @@ import humps
 
 class CamelCaseJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        # if data is not None:
-        #     data = humps.camelize(data)
+        if data is not None:
+            data = humps.camelize(data)
         return super().render(data, accepted_media_type, renderer_context)
