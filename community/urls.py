66a6961ca7b5ac5ed3a1f2fa1437ef75bedc2f43
hyunyoung97 <gusdud04164@gmail.com>
2025-05-19 13:33:50 +0900

    수정

--- community/urls.py
+++ community/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CommunityListCreateView, CommunityDetailView
+from .views import CommunityListCreateView, CommunityDetailView, CommunityLikeToggleView
 
 router = DefaultRouter()
 router.register(r'', CommunityListCreateView, basename='community')
 
 urlpatterns = [
     path('', include(router.urls)),  # ✅ router만 사용
+    # 게시글 상세/수정/삭제
     path('<uuid:community_uuid>/', CommunityDetailView.as_view(), name='community-detail'),
+    path('<uuid:communtiy_uuid>/like/', CommunityLikeToggleView.as_view(), name='community-like'),
 ]
