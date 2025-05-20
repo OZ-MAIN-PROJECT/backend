806e9f882ac3c80a7644794a50e36b33956098d1
miu1217 <hyun671217@daum.net>
2025-05-20 17:31:49 +0900

    base.py S3로 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,13 +38,14 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 
 # 이 부분이 핵심!
-AWS_S3_CUSTOM_DOMAIN = os.getenv('CLOUDFRONT_DOMAIN')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # 파일 업로드 설정
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIAFILES_LOCATION = ''
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
+AWS_DEFAULT_ACL = 'public-read'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
