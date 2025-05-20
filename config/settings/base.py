cbab09eb76eb45592e717c425fbc6a0a9294de90
miu1217 <hyun671217@daum.net>
2025-05-20 17:33:50 +0900

    base.py S3로 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,7 +45,6 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-AWS_DEFAULT_ACL = 'public-read'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
